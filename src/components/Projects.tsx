f43aec8267afc4d8f194996d1d626cb06108f33e
redberry0217 <redberry0217@gmail.com>
2025-06-11 17:20:37 +0900

    feat: 윈터뷰 프로젝트 내용 완성

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -50,7 +50,7 @@ const Projects = () => {
             </span>
           </div>
           <div className="flex flex-col gap-[10px]">
-            <span className="text-xl font-bold">[담당 역할]</span>
+            <span className="text-xl font-bold">[상세 내용]</span>
             <div className="flex flex-col gap-[10px] pb-[15px]">
               <span className="text-lg font-bold">
                 사용자 경험 개선을 위한 단계별 페이지 라우팅
@@ -59,14 +59,6 @@ const Projects = () => {
                 <li>
                   원하는 조건 선택 → 조건에 맞는 멘토 검색 결과 확인 → 선택한
                   멘토와 스케줄 조율 및 최종 결제
-                </li>
-                <li>
-                  한 페이지에서 처리하지 않고 3개의 단계별 페이지로 라우팅하고,
-                  모달 병행해 구현
-                </li>
-                <li>
-                  멘토 결과 페이지에서 멘토별 상세 정보는 모달 활용하여 화면
-                  전환 없이 빠르게 확인 가능하도록 구현
                 </li>
                 <li>
                   사용자가 비로그인 상태인 경우 로그인페이지로 이동, 로그인 완료
@@ -81,27 +73,167 @@ const Projects = () => {
               </span>
               <span className="flex flex-col gap-[5px] text-lg leading-[1.5]">
                 <li>
-                  (1)채팅 로그 UI 렌더링에 필요한 상태와 (2)서버에 전달될 실제
+                  ①채팅 로그 UI 렌더링에 필요한 상태와 ②서버에 전달될 실제
                   사용자 입력값 관리에 필요한 상태를 각각 zustand 전역 상태로
-                  관리하여 페이지 이동 시에도 기존 입력값과 채팅 로그가 유지되어
+                  관리하여 페이지 이동 시에도 기존 입력값과 채팅 로그가 유지되고
                   사용자가 흐름을 이어갈 수 있도록 구현
                 </li>
-                <li>type</li>
               </span>
             </div>
+          </div>
+          <Divider gap={10} />
+          <span className="text-2xl font-bold">
+            멘토의 면접 리포트 작성 기능, 멘티의 리포트 조회 기능 개발
+          </span>
+          <span className="text-xl font-bold">2024.09 - 2024.10</span>
+          <div className="flex flex-col gap-[10px]">
+            <span className="text-lg leading-[1.5]">
+              리포트 작성은 과정이 길고 응답 항목이 많아 작성 도중 데이터가
+              유실되거나 사용자가 현재 진행 상황을 직관적으로 파악하기 어려울 수
+              있습니다. 이에 임시저장, 불러오기, 진행상황 표시, 문항 비활성화
+              토글 기능을 구현하여 작성 흐름을 최적화했습니다.
+            </span>
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <span className="text-xl font-bold">[상세 내용]</span>
+            <div className="flex flex-col gap-[10px] pb-[15px]">
+              <span className="text-lg font-bold">리포트 작성 구현</span>
+              <span className="flex flex-col gap-[5px] text-lg leading-[1.5]">
+                <li>
+                  isFinal값 활용하여 작성 내용을 서버에 임시저장, 최초 렌더링시
+                  useEffect로 저장 데이터 불러오거나 저장 내용이 없는 경우
+                  초기화
+                </li>
+                <li>입력값 제한과 유효성 검증으로 데이터 안정성 강화</li>
+              </span>
+            </div>
+            <div className="flex flex-col gap-[10px] pb-[15px]">
+              <span className="text-lg font-bold">리포트 조회 구현</span>
+              <span className="flex flex-col gap-[5px] text-lg leading-[1.5]">
+                <li>
+                  로딩, 캐싱 및 에러 상태를 직관적으로 처리할 수 있는 Tanstack
+                  Query를 사용해 서버에서 리포트 데이터 조회, 사용자가 쉽게
+                  이해할 수 있도록 구조화된 리포트 UI 구현
+                </li>
+              </span>
+            </div>
+          </div>
+          <Divider gap={10} />
+          <span className="text-2xl font-bold">
+            사용자 맞춤형 기능 설계 밎 구현
+          </span>
+          <span className="text-xl font-bold">2024.09 - 2025.10</span>
+          <div className="flex flex-col gap-[10px]">
+            <span className="text-lg leading-[1.5]">
+              사용자 경험을 최우선으로 고려해 주요 기능을 구현했습니다. 멘토
+              필터링에서는 사용자가 어떤 조건으로 검색해야 자신의 니즈에
+              부합하는 결과를 얻을 수 있을지를 중점적으로 고민했습니다. 또한,
+              멘토 리스트와 상세 페이지에서 관심 있는 멘토를 북마크하고, 북마크
+              페이지에서 이를 한눈에 모아서 볼 수 있도록 구현해 사용자 편의성을
+              높였습니다.
+            </span>
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <span className="text-xl font-bold">[상세 내용]</span>
+            <div className="flex flex-col gap-[10px] pb-[15px]">
+              <span className="text-lg font-bold">
+                멘토 검색 필터링, 멘토 북마크 기능
+              </span>
+              <span className="flex flex-col gap-[5px] text-lg leading-[1.5]">
+                <li>
+                  실시간 검색 입력 시 성능 저하 및 사용자 경험 저하 가능성이
+                  있으므로, 디바운싱 적용해 불필요한 API 호출을 줄이고 검색
+                  성능을 개선
+                </li>
+                <li>
+                  북마크 추가/삭제 시 인터페이스에 지연이 발생하거나 데이터
+                  불일치 문제를 방지하여 안정적인 경험을 제공하기 위해 낙관적
+                  업데이트 적용
+                </li>
+              </span>
+            </div>
+            <div className="flex flex-col gap-[10px] pb-[15px]">
+              <span className="text-lg font-bold">
+                마이페이지 : (멘티)리뷰 작성하기, (멘토)정산 리스트
+              </span>
+            </div>
+          </div>
+          <Divider gap={10} />
+          <span className="text-2xl font-bold">
+            운영 및 관리 기능 설계 및 구현
+          </span>
+          <span className="text-xl font-bold">2024.11 - 진행중</span>
+          <div className="flex flex-col gap-[10px]">
+            <span className="text-lg leading-[1.5]">
+              관리자와 사용자 간의 원활한 소통과 서비스 운영의 효율성을
+              증진시키는 고객센터 페이지와 관리자 페이지를 개발했습니다.
+              고객센터 페이지에 있는 공지사항이나 자주묻는질문(FAQ)는 관리자
+              페이지에서 작성, 등록, 삭제, 공개 설정이 가능합니다. 특히 관리자
+              페이지는 권한이 있는 사용자만 접근할 수 있도록 제어하고, 세션을
+              안정적으로 유지하는 것이 중요했습니다. 관리자 페이지 기능 기획과
+              구현을 현재까지 진행 중에 있습니다.
+            </span>
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <span className="text-xl font-bold">[상세 내용]</span>
+            <div className="flex flex-col gap-[10px] pb-[15px]">
+              <span className="text-lg font-bold">고객센터 페이지</span>
+              <span className="flex flex-col gap-[5px] text-lg leading-[1.5]">
+                <li>
+                  공지사항 항목이 많아질 수 있으므로 페이지네이션 적용하여
+                  데이터를 단계적으로 불러오도록 적용, 공지 상세내용에서 뒤로
+                  가기 시 보고 있던 페이지를 유지하기 위해 쿼리 파라미터에
+                  페이지 표시
+                </li>
+                <li>
+                  문의하기 페이지에서 첨부파일 업로드 시 보안과 서버 부하 문제를
+                  해결하기 위해 서버가 발급한 presignedUrl을 활용하여
+                  클라이언트가 S3에 파일을 업로드하도록 구현
+                </li>
+              </span>
+            </div>
+            <div className="flex flex-col gap-[10px] pb-[15px]">
+              <span className="text-lg font-bold">관리자 페이지</span>
+              <span className="flex flex-col gap-[5px] text-lg leading-[1.5]">
+                <li>
+                  최상위 layout에 로그인 상태와 사용자 역할을 확인 후 리디렉션
+                  처리하는 커스텀 훅 적용
+                </li>
+                <li>
+                  API 호출 응답으로 accessToken을 받고, refreshToken은 쿠키에
+                  저장하여 로그인 유지지
+                </li>
+                <li>
+                  별도 디자인 없이 Headless UI 활용하여 통일성 있는 UI를
+                  효율적으로 구현
+                </li>
+              </span>
+            </div>
+            <Divider gap={10} />
           </div>
           <div className="flex flex-col gap-[10px]">
             <span className="text-xl font-bold">[성과 및 배운점]</span>
             <span className="flex flex-col gap-[5px] text-lg leading-[1.5]">
               <li>
-                업무 몰입도, 원활한 커뮤니케이션 능력, 문제 발견 및 개선 역량
-                우수 평가
+                Next.js 환경의 비즈니스 로직 개발 경험, REST API를 통해 백엔드와
+                효과적인 상호작용
               </li>
               <li>
-                유연하고 확장 가능하며 협업과 사용성을 고려한 공통 컴포넌트 개발
-                경험
+                다양한 문제를 정의하고, 이를 해결하기 위한 최적의 기술 스택과
+                로직 설계 경험
               </li>
-              <li>프로젝트 기획 및 아이데이션 역량 강화</li>
+              <li>
+                로컬/전역 state 관리와 Tanstack Query를 활용한 데이터 캐싱,
+                에러처리, 쿼리 키에 대한 이해도 향상
+              </li>
+              <li>
+                Desktop, Tablet, Mobile 환경에서 일관된 사용자 경험 제공을 위한
+                반응형 디자인 적용
+              </li>
+              <li>
+                백엔드, 디자이너와 협업하며 커뮤니케이션 역량 향상, 비정기적
+                코드 리뷰를 통해 코드 품질 향상 경험
+              </li>
             </span>
           </div>
           <div className="flex flex-col gap-[10px]">
@@ -167,7 +299,7 @@ const Projects = () => {
             </span>
           </div>
           <div className="flex flex-col gap-[10px]">
-            <span className="text-xl font-bold">[담당 역할]</span>
+            <span className="text-xl font-bold">[상세 내용]</span>
             <div className="flex flex-col gap-[10px] pb-[15px]">
               <span className="text-lg font-bold">
                 퀴즈 만들기 에디터 기능 개발
