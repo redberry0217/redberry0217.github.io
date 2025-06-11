c111bc6526636c810055376d67aa7079693171be
redberry0217 <redberry0217@gmail.com>
2025-06-11 12:25:29 +0900

    feat: 배포 에러 수정중

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "",
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: true,
-  },
+  publicDir: "public",
 });
